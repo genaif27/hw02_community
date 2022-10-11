3698ef44e56d7a490852e0b848f5c226db89cdcf
Gennady Lee <gennadylee@gmail.com>
2022-10-11 16:09:57 +0300

    last hope

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,18 +3,19 @@ from .models import Post, Group
 
 
 def index(request):
-    template = 'posts/index.html'
-    latest_posts = Post.objects.order_by("-pub_date")[:12]
+    
+    latest_posts = Post.objects.all()
     context = {
         'posts': latest_posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_list(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:10]
+    # posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     # В словаре context отправляем информацию в шаблон
     context = {
         'group': group,
